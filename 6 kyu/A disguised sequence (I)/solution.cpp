2773dd3594e842a630a488b8c196b8bde30fbc41
a3amat1K <81564718+a3a256@users.noreply.github.com>
2025-01-26 12:36:15 +0000

    solved "A disguised sequence (I)"

---
+++ 6 kyu/A disguised sequence (I)/solution.cpp
@@ -0,0 +1,10 @@
+#include <math.h>
+
+class HiddenSeq
+{
+    public:
+      static unsigned long long fcn(int n){
+        unsigned long long ans = std::pow(2, n);
+        return ans;
+      }
+};
